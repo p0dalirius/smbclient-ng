84bd7b37724f877cef16c8efe836475ec60c9e2c
Remi GASCOU (Podalirius) <79218792+p0dalirius@users.noreply.github.com>
2024-05-24 20:49:34 +0200

    Refactored as package

---
+++ smbclient_ng/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File name          : __init__.py
+# Author             : Podalirius (@podalirius_)
+# Date created       : 23 may 2024
+
+from .core import *
+from .modules import *
