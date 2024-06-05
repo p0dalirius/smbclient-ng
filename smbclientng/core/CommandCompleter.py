0f5dd34b24ec16786230febc0f013ae30c825d73
Rémi GASCOU (Podalirius) <podalirius@protonmail.com>
2024-06-05 15:50:19 +0200

    Add command sizeof (#20)
    * Created get_entry() function
    
    * Created 'sizeof' command

--- smbclientng/core/CommandCompleter.py
+++ smbclientng/core/CommandCompleter.py
@@ -61,6 +61,13 @@ class CommandCompleter(object):
             ], 
             "subcommands": []
         },
+        "debug": {
+            "description": [
+                "Command for dev debugging.",
+                "Syntax: 'debug'"
+            ], 
+            "subcommands": []
+        },
         "dir": {
             "description": [
                 "List the contents of the current working directory.",
@@ -208,6 +215,13 @@ class CommandCompleter(object):
             ], 
             "subcommands": []
         },
+        "sizeof": {
+            "description": [
+                "Recursively compute the size of a folder.", 
+                "Syntax: 'sizeof [directory|file]'"
+            ], 
+            "subcommands": []
+        },
         "shares": {
             "description": [
                 "Lists the SMB shares served by the remote machine.", 
@@ -299,7 +313,7 @@ class CommandCompleter(object):
                                 if s.lower().startswith(remainder.lower())
                             ]
 
-                        elif command in ["bat", "cat", "get", "rm"]:
+                        elif command in ["bat", "cat", "debug", "get", "rm"]:
                             # Choose local files and directories
                             path = ""
                             if '\\' in remainder.strip() or '/' in remainder.strip():
