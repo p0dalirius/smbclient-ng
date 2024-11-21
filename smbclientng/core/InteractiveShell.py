ebcf32ff89dfba6bd61dc6701394f03ba246fd9c
Jordan Ovrè <ghecko78@gmail.com>
2024-11-21 10:25:53 +0100

    Fix file deletion (#115)
    Co-authored-by: jordan <jordan.ovre@elca.ch>

--- smbclientng/core/InteractiveShell.py
+++ smbclientng/core/InteractiveShell.py
@@ -862,12 +862,19 @@ class InteractiveShell(object):
         # SMB share needed             : Yes
 
         for path_to_file in arguments:
-            # Wildcard
+            # Check if the path is absolute
+            # Fullpath is required to check if path is a file
+            if ntpath.isabs(path_to_file):
+                full_path = ntpath.normpath(path_to_file)
+            else:
+                # Relative path, construct full path
+                full_path = ntpath.normpath(ntpath.join(self.sessionsManager.current_session.smb_cwd, path_to_file))
+            # Wildcard handling
             if '*' in path_to_file:
                 self.sessionsManager.current_session.rm(path=path_to_file)
             # File
             elif self.sessionsManager.current_session.path_exists(path_to_file):
-                if self.sessionsManager.current_session.path_isfile(path_to_file):
+                if self.sessionsManager.current_session.path_isfile(full_path):
                     try:
                         self.sessionsManager.current_session.rm(path=path_to_file)
                     except Exception as e:
