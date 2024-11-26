774a7e36f981b4ffeb06fef5c0062179e0d8ad34
Roman Karwacik <roman.karwacik@redteam-pentesting.de>
2024-11-26 10:06:47 +0100

    Fix lbat and lcat

--- smbclientng/core/InteractiveShell.py
+++ smbclientng/core/InteractiveShell.py
@@ -489,7 +489,7 @@ class InteractiveShell(object):
         # SMB share needed             : No
 
         # Parse wildcards
-        files_and_directories = resolve_remote_files(self.sessionsManager.current_session, arguments)
+        files_and_directories = resolve_local_files(arguments)
 
         for path_to_file in files_and_directories:
             # Read the file
@@ -527,7 +527,7 @@ class InteractiveShell(object):
         # SMB share needed             : No
 
         # Parse wildcards
-        files_and_directories = resolve_remote_files(self.sessionsManager.current_session, arguments)
+        files_and_directories = resolve_local_files(arguments)
 
         for path_to_file in files_and_directories:
             # Read the file 
